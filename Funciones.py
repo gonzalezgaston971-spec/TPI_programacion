0eab58f0d180a7b86cd4dc829653a5d2291b5977
gonzalezgaston971-spec <gonzalezgaston971@gmail.com>
2026-05-23 03:07:09 -0300

    Agregar menú principal y estructura de control en Main_tpi.py

---
+++ Funciones.py
@@ -0,0 +1,18 @@
+
+def menu_principal():
+    print("""
+
+================ MENU =================
+
+1. Agregar paises
+2. Actualizar poblacion y superficie
+3. Buscar un pais
+4. Filtrar paises
+5. Ordenar paises
+6. Mostrar estadisticas 
+7. Salir \n""")
+    print("="*40)
+    opcion = input("\nIngrese su opcion aqui por favor: ").strip().lower()
+    print("")
+    print("="*40)
+    return opcion
---
+++ Main_tpi.py
@@ -0,0 +1,24 @@
+from Funciones import *
+#Llamamos todas las funciones existentes en el archivo de funciones a la rama principal
+
+while True:
+    opcion = menu_principal()
+    match opcion:
+        case "1" | "uno":
+            pass
+        case "2" | "dos":
+            pass
+        case "3" | "tres":
+            pass
+        case "4" | "cuatro":
+            pass
+        case "5" | "cinco":
+            pass
+        case "6" | "seis":
+            pass
+        case "7" | "siete":
+            print("Cerrando menu, Esperamos fuera de su agrado")
+            print("¡Adios!")
+            break
+        case _:
+            print("\nPor favor ingrese una de las opciones dadas en el 'MENU'")
